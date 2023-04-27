d27e173156ff01ca46651210c13bf33a4b838c8c
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2023-04-27 16:28:08 +0700

    refactor file location

--- server/index.js
+++ server/index.js
@@ -7,3 +7,8 @@ require("dotenv").config();
 
 app.use(cors())
 app.use(express.json())
+
+const server = app.listen(process.env.PORT, () =>{
+
+    console.log('Server Started on Port ${process.env.PORT}');
+});
