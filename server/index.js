8c51f1bee5b3c93391406fc6db0226af4a467bdc
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2023-04-27 16:45:21 +0700

    setup-database

--- server/index.js
+++ server/index.js
@@ -5,10 +5,18 @@ const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    console.log(`DB connection Succesfull`);
+}).catch((err)=>{
+    console.log(err.message);
+});
 
 const server = app.listen(process.env.PORT, () =>{
-
-    console.log('Server Started on Port ${process.env.PORT}');
+    console.log(`Server Started on Port ${process.env.PORT}`);
 });
