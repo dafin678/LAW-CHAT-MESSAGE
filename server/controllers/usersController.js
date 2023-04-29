15b7e40b4507b2a9f1d9a8b91ca77d3f724c17b4
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2023-04-29 15:25:29 +0700

    implement set avatar choices

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -25,6 +25,8 @@ module.exports.register = async (req, res, next) => {
 
   
 
+  
+
 module.exports.login = async (req, res, next) => {
   try {
     const { username, password } = req.body;
@@ -36,6 +38,27 @@ module.exports.login = async (req, res, next) => {
       return res.json({ msg: "Incorrect Username or Password", status: false });
     delete user.password;
     return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      {
+        isAvatarImageSet: true,
+        avatarImage,
+      },
+      { new: true }
+    );
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
   } catch (ex) {
     next(ex);
   }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,14 @@
+
 const {
     login,
     register,
+    setAvatar,
   } = require("../controllers/usersController");
   
   const router = require("express").Router();
   
   router.post("/login", login);
   router.post("/register", register);
+  router.post("/setAvatar/:id", setAvatar)
   
   module.exports = router;
