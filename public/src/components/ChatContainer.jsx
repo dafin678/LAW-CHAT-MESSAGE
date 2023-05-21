cfc103a3032cdd93d1fbc17425a067e5f70e6fe3
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2023-05-21 16:04:24 +0700

    install-socket

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -66,6 +66,13 @@ export default function ChatContainer({ currentChat,currentUser }) {
 
 const Container = styled.div`
   padding-top: 1rem;
+  display: grid;
+  grid-template-rows: 10% 78% 12%;
+  gap: 0.1rem;
+  overflow: hidden;
+  @media screen and (min-width: 720px) and (max-width: 1080px) {
+    grid-template-rows: 15% 70% 15%;
+  }
   .chat-header {
     display: flex;
     justify-content: space-between;
@@ -107,6 +114,15 @@ const Container = styled.div`
     }
     .sended{
       justify-content: flex-end;
+      .content{
+        background-color: #4f04ff21;
+      }
+    }
+    .received{
+      justify-content: flex-start;
+      .content{
+        background-color: #9900ff20;
+      }
     }
   }
 `;
