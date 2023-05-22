8730aa7662ae112cac69ed2033f57b30a24ec45b
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2023-05-23 00:13:50 +0700

    decompose-services

--- broadcast-service/routes/broadcastRoutes.js
+++
@@ -1,6 +0,0 @@
-const { getBroadcastMessage} = require("../controllers/broadcastController");
-const router = require("express").Router();
-
-router.post("/get/", getBroadcastMessage);
-
-module.exports = router;
--- contact-service/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const {addContact, deleteContact, getContactList} = require("../controllers/contactControllers");
-
-const router = require("express").Router();
-
-router.post("/api/contact/:user", addContact);
-router.delete("/api/contact/:user", deleteContact);
-router.get("/api/contact/:user", getContactList);
-
-module.exports = router;
