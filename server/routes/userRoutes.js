37eb839f3388473cc2a6d4573fefe3c8f9df0cfb
Muhammad Dhafin Fauzan <muhammad.dhafin01@ui.ac.id>
2023-05-23 00:14:58 +0700

    Dafin send message

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,16 @@
+
+const {
+    login,
+    register,
+    setAvatar,
+    getAllUsers,
+  } = require("../controllers/usersController");
+  
+  const router = require("express").Router();
+  
+  router.post("/login", login);
+  router.post("/register", register);
+  router.post("/setAvatar/:id", setAvatar);
+  router.get("/allusers/:id", getAllUsers);
+  
+  module.exports = router;
